3b842e38239c6678cfb3848ed6ea546fde907e44
johnny21-9 <heartless6m@gmail.com>
2021-08-03 03:53:56 +0300

    hw-12

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,27 @@
+export default class ApiCountries {
+  constructor() {
+    this.searchQuery = '';
+    
+  }
+  fetchCountries(url) {
+    const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+      const params = "?fields=;name;capital;population;flag;languages;"
+    return fetch(`${BASE_URL}/${this.searchQuery}?fields=${this.name}${params}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+        }
+ 
+      return response.json();
+    }
+  )
+  .catch(error => console.log(error));
+  }
+  get query() {
+    return this.searchQuery ;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery
+    return this.searchQuery
+  }
+}
---
+++ src/js/searchCountry.js
@@ -0,0 +1,49 @@
+const DEBOUNCE_DELAY = 300;
+import ApiCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import countryItemTpl from '../templates/country-card.hbs';
+import countryListTpl from '../templates/country-list.hbs';
+import Notiflix from "notiflix";
+const apiCountries = new ApiCountries();
+
+const searchElement = document.getElementById("search-box");
+const listElement = document.querySelector(".country-list");
+const infoElement = document.querySelector(".country-info");
+
+searchElement.addEventListener('input', debounce((e) => {
+    apiCountries.query = searchElement.value;
+    
+    apiCountries.fetchCountries()
+        .then((countries) => {
+       
+            if (countries.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+
+            if (countries.length === 1) {
+                renderCountryCard(countries);
+            }
+
+            if (countries.length >= 2 && countries.length <= 10) {
+                renderCountryList(countries);
+            }
+             
+        })
+        .catch(error =>  onReject());
+
+}, DEBOUNCE_DELAY));
+function renderCountryCard(countries) {
+  clearCountryCard()
+    infoElement.insertAdjacentHTML('afterbegin', countryItemTpl(countries));
+}
+function renderCountryList(countries) {
+  clearCountryCard()
+    infoElement.insertAdjacentHTML('afterbegin', countryListTpl(countries));
+}
+function clearCountryCard() {
+    infoElement.innerHTML = '';
+}
+function onReject() {
+    clearCountryCard();
+    
+Notiflix.Notify.failure('Oops, there is no country with that name'); }
